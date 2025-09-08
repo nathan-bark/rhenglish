92e5d2b83b182663fa5b55f2950ce1ad9ed7f405
Nathan <nathanbark4@gmail.com>
2025-09-08 15:44:09 +0300

    form update

--- src/routes/ContactPage/ContactPage.component.jsx
+++ src/routes/ContactPage/ContactPage.component.jsx
@@ -16,7 +16,7 @@ const ContactPage = () => {
 
   const handleChange = (e) => {
     console.log(formData.name, formData.email, formData.message);
-    
+
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
@@ -28,7 +28,7 @@ const ContactPage = () => {
       "https://script.google.com/macros/s/AKfycby-Ah3ZmKLxFgnKtz98BdGvupOqfWO3pOIMk3efxlkTHocmgEi28HJW5EmgY42giMRj/exec"; // Replace with your Google Sheets URL
 
     try {
-       await fetch(sheetsURL, {
+      await fetch(sheetsURL, {
         method: "POST",
         mode: "no-cors",
         headers: {
@@ -36,9 +36,8 @@ const ContactPage = () => {
         },
         body: JSON.stringify(formData),
       });
-        setStatus("Message sent successfully!");
-        setFormData({ name: "", email: "", message: "" });
-      
+      setStatus("Message sent successfully!");
+      setFormData({ name: "", email: "", message: "" });
     } catch (error) {
       console.error("Error submitting form:", error);
       setStatus("There was an error sending your message.");
@@ -60,75 +59,87 @@ const ContactPage = () => {
 
       <div className="bg-surface p-8 rounded-lg shadow-md">
         <p className="text-lg text-center mb-6 text-textPrimary">
-          Have a question or want to book a lesson? Please get in touch using
-          one of the methods below. A contact form will be added here soon!
+          Have a question or want to book a lesson? Please send us a message
+          using the form and we will contact you within 24 hours.
         </p>
         {/* Contact Form */}
         <form onSubmit={handleSubmit} className="space-y-4 max-w-md mx-auto">
           <div>
-            <label htmlFor="name" className="block text-sm font-medium text-textPrimary">Name</label>
+            <label
+              htmlFor="name"
+              className="block text-sm font-medium text-textPrimary"
+            >
+              Name
+            </label>
             <input
-              type="text" 
+              type="text"
               id="name"
               name="name"
               value={formData.name}
               onChange={handleChange}
-              className="mt-1 block w-full rounded-md bg-background border-gray-300 shadow-sm focus:border-accent focus:ring-accent-500 sm:text-sm p-2"
+              className="mt-1 block w-full rounded-md bg-white border-gray-300 shadow-sm focus:outline-accent focus:border-accent focus:ring-accent sm:text-sm p-2"
               required
             />
           </div>
           <div>
-            <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-textPrimary"
+            >
+              Email
+            </label>
             <input
               type="email"
               id="email"
               name="email"
               value={formData.email}
               onChange={handleChange}
-              className="mt-1 block w-full rounded-md  bg-background border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm p-2"
+              className="mt-1 block w-full rounded-md  bg-white border-gray-300 shadow-sm focus:outline-accent focus:border-accent focus:ring-accent sm:text-sm p-2"
               required
             />
           </div>
           <div>
-          <label htmlFor="message" className="block text-sm font-medium text-gray-700">Message</label>
-          <textarea 
-            id="message" 
-            name="message" 
-            rows="4" 
-            value={formData.message}
-            onChange={handleChange}
-            className="mt-1 block w-full rounded-md bg-background border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm p-2" 
-            required 
-          ></textarea>
-        </div>
-        <div className="flex justify-end">
-          <button 
-            type="submit" 
-            className="w-full bg-primary hover:bg-secondary text-white font-bold py-3 px-8 rounded-full shadow-lg transform hover:scale-105 transition-all duration-300">
-            Send Message
-          </button>
-        </div>
+            <label
+              htmlFor="message"
+              className="block text-sm font-medium text-textPrimary"
+            >
+              Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              rows="4"
+              value={formData.message}
+              onChange={handleChange}
+              className="mt-1 block w-full rounded-md bg-white border-gray-300 shadow-sm focus:outline-accent focus:border-accent focus:ring-accent sm:text-sm p-2"
+              required
+            ></textarea>
+          </div>
+          <div className="flex justify-end">
+            <button
+              type="submit"
+              className="w-full bg-primary hover:bg-secondary text-textOnDark font-bold py-3 px-8 rounded-full shadow-lg transform hover:scale-105 transition-all duration-300"
+            >
+              Send Message
+            </button>
+          </div>
         </form>
-         {status && (
-        <div className={`mt-4 text-center ${status.includes('successfully') ? 'text-success' : 'text-error'}`}>
-          {status}
-        </div>
-      )}
+        {status && (
+          <div
+            className={`mt-4 text-center ${
+              status.includes("successfully") ? "text-success" : "text-error"
+            }`}
+          >
+            {status}
+          </div>
+        )}
 
         {/* Direct contact info section */}
         <div className="mt-8 text-center">
-          <p className="text-textPrimary">You can reach us directly at:</p>
-          <a
-            href="mailto:rhenglishuk@gmail.com"
-            className="text-primary font-semibold hover:underline"
-          >
-            rhenglishuk@gmail.com
-          </a>
-
           {/* Social Media Links Section */}
           <div className="mt-4">
             <h4 className="text-lg font-semibold text-textPrimary mb-2">
-              Connect with me on social media:
+              Or connect with us on social media:
             </h4>
             <div className="flex justify-center space-x-4">
               {socialMediaLinks.map((link) => (
